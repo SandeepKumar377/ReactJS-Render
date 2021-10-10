54bc6d82502638c2fb2e46b0838c205ffb5e93f1
Sandeep Kumar <sandeepkumar.sk5.ss55@gmail.com>
2021-10-11 03:06:38 +0530

    useReducerExceptionWhenUpdateStateToTheSameValueAsTheCurrentState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import { UseState } from './components/UseState/UseState';
+import { UseReducer } from './components/UseReducer/UseReducer';
+// import { UseState } from './components/UseState/UseState';
 
 function App() {
   return (
     <div className="App">
-      <UseState />
+      <UseReducer />
+      {/* <UseState /> */}
     </div>
   );
 }
