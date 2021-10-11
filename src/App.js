7ead2d2a842b87ee8ffa1ba98e8802cd3d403eb9
Sandeep Kumar <sandeepkumar.sk5.ss55@gmail.com>
2021-10-11 17:09:31 +0530

    Parent_and_Child_Render

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
-import { ArrayUseState } from './components/Immutable State/ArrayUseState';
-import { ObjectUseState } from './components/Immutable State/ObjectUseState';
+import { Parent } from './components/Parent Child/Parent';
+// import { ArrayUseState } from './components/Immutable State/ArrayUseState';
+// import { ObjectUseState } from './components/Immutable State/ObjectUseState';
 // import { UseReducer } from './components/UseReducer/UseReducer';
 // import { UseState } from './components/UseState/UseState';
 
 function App() {
   return (
     <div className="App">
-      <ObjectUseState />
-      <ArrayUseState />
+      <Parent/>
+      {/* <ObjectUseState />
+      <ArrayUseState /> */}
       {/* <UseReducer /> */}
       {/* <UseState /> */}
     </div>
